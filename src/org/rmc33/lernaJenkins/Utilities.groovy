10e178f7336df35786b77248e298c74b780f65bc
Ryan Conigliaro <ryan.conigliaro@chase.com>
2020-03-30 02:41:42 -0400

    updating import

--- src/org/rmc33/lernaJenkins/Utilities.groovy
+++ src/org/rmc33/lernaJenkins/Utilities.groovy
@@ -4,9 +4,9 @@ class Utilities {
 
    static def findChangedPackages(steps, targetBranch) {
         def changedPackages = new HashSet<String>();
-        steps.checkout scm
+        steps.checkout steps.scm
         String diffFilesList = steps.sh(script: "git diff remotes/origin/${targetBranch} --name-only", returnStdout: true)
-        steps.echo "change branch ${env.CHANGE_BRANCH} diff : ${diffFilesList}-"
+        steps.echo "change branch ${steps.env.CHANGE_BRANCH} diff : ${diffFilesList}-"
         List<String> files = Arrays.asList(diffFilesList.split("\\r?\\n"))
         files.each { file ->
             def matcher = file =~ /packages\/(.*?)\//
