5ffa0a44340adbb9db53f6092ced4e067f55887e
Ryan Conigliaro <rc@Ryans-MacBook-Pro.local>
2020-04-21 23:56:23 -0400

    edit to develop pipeline

--- jenkins/develop-pipeline.groovy
+++ jenkins/develop-pipeline.groovy
@@ -2,7 +2,7 @@ import org.rmc33.lernaJenkins.Utilities
 
 def listChangedPackages(steps) {
     steps.echo "getChangedPackages"
-    def releaseVersion = steps.sh script: "node -p -e \"require('./package.json').version\""
+    def releaseVersion = steps.sh (script: "node -p -e \"require('./package.json').version\"", returnStdout: true)
     return Utilities.listChangedPackagesGitDiff(steps, "remotes/origin/release/${releaseVersion}")
 }
 
