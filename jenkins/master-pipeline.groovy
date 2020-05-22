49ee3477fec96c9f7b2cbf9b90f2a488dd7a06ec
Ryan Conigliaro <rc@Ryans-MacBook-Pro.local>
2020-05-22 12:50:27 -0400

    updating version

--- jenkins/master-pipeline.groovy
+++ jenkins/master-pipeline.groovy
@@ -7,18 +7,21 @@ def listChangedPackages(steps) {
 }
 
 def runBeforePackagesPipeline(script) {
+    script.sh "git checkout master"
     script.sh "yarn"
 }
 
 def runPackagePipeline(script, packageName) {
     script.echo "runPipeline ${packageName}"
-    //scan
+    script.dir("packages/${packageName}") {
+        def releaseVersion = steps.sh (script: "node -p -e \"require('./package.json').version\"", returnStdout: true)
+        script.sh "git tag ${packageName}@${releaseVersion}"
+    }
 }
 
 def runAfterPackagesPipeline(script) {
     //lerna publish and deploy to prod
     withCredentials([usernamePassword(credentialsId: 'GITHUB_USER', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        script.sh "git checkout master"
         def releaseVersion = steps.sh (script: "node -p -e \"require('./package.json').version\"", returnStdout: true)
         script.sh "git tag $releaseVersion"
         script.sh 'git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/rmc33/lernaJenkins.git --tags'
