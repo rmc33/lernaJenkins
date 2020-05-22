49951aa17c422f99658e24d6ab37a98b73aa165c
Ryan Conigliaro <rc@Ryans-MacBook-Pro.local>
2020-05-22 12:41:12 -0400

    updating version

--- jenkins/develop-pipeline.groovy
+++ jenkins/develop-pipeline.groovy
@@ -19,7 +19,7 @@ def runPackagePipeline(script, packageName) {
     script.dir("packages/${packageName}") {
         withCredentials([usernamePassword(credentialsId: 'GITHUB_USER', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
             //bump up package version (should also get user input for version number)
-            script.sh "yarn --new-version patch -m 'updating version' --no-git-tag-version"
+            script.sh "yarn version --no-git-tag-version --new-version patch"
             script.sh 'git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/rmc33/lernaJenkins.git'
         }
     }
@@ -31,7 +31,7 @@ def runAfterPackagesPipeline(script) {
     script.sh "yarn config set version-git-message 'updating version'"
     //bump up repo version (should also get user input for version number)
     withCredentials([usernamePassword(credentialsId: 'GITHUB_USER', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        script.sh "yarn --new-version patch --no-git-tag-version"
+        script.sh "yarn version --no-git-tag-version --new-version patch"
         script.sh 'git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/rmc33/lernaJenkins.git'
         script.sh "git checkout -b release/${newVersion}"
         script.sh "git push origin release/${newVersion}"
