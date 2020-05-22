d1b31ecc6e2310caacfb2de2da6c275c73868a61
Ryan Conigliaro <rc@Ryans-MacBook-Pro.local>
2020-05-21 21:39:16 -0400

    updating version

--- jenkins/develop-pipeline.groovy
+++ jenkins/develop-pipeline.groovy
@@ -14,20 +14,24 @@ def runBeforePackagesPipeline(script) {
 def runPackagePipeline(script, packageName) {
     script.echo "runPipeline ${packageName}"
     script.dir("packages/${packageName}") {
-        //bump up package version (can also ask for user input on version number)
-        script.sh "npm version patch -m 'updating version'"
-        script.sh "git push"
+        withCredentials([usernamePassword(credentialsId: 'GITHUB_ID', usernameVariable: 'username', passwordVariable: 'password')]){
+            //bump up package version (can also ask for user input on version number)
+            script.sh "npm version patch -m 'updating version'"
+            sh("git push https://${username}:${password}@ggithub.com/rmc33/lernaJenkins.git")
+        }
     }
 }
 
 def runAfterPackagesPipeline(script) {
     script.echo "create release after develop build"
     //bump up repo version (can also ask for user input on version number)
-    def newVersion = script.sh (script: "npm version patch", returnStdout: true)
-    script.sh "npm version patch -m 'updating version'"
-    script.sh "git push"
-    script.sh "git checkout -b release/${newVersion}"
-    script.sh "git push origin release/${newVersion}"
+    withCredentials([usernamePassword(credentialsId: 'GITHUB_ID', usernameVariable: 'username', passwordVariable: 'password')]){
+        def newVersion = script.sh (script: "npm version patch", returnStdout: true)
+        script.sh "npm version patch -m 'updating version'"
+        script.sh "git push"
+        script.sh "git checkout -b release/${newVersion}"
+        script.sh "git push origin release/${newVersion}"
+    }
 }
 
 return this;
