19bb3e3cdaeda037d21d7c7eec72508f340733be
Ryan Conigliaro <rc@RyansMac.local>
2020-10-02 22:28:31 -0400

    asking user input for version updates

--- src/org/rmc33/lernaJenkins/GitUtilities.groovy
+++ src/org/rmc33/lernaJenkins/GitUtilities.groovy
@@ -2,23 +2,23 @@ import groovy.json.JsonSlurper
 
 class GitUtilities {
 
-   static def listChangedFiles(steps, targetBranch) {
+   static def listChangedFiles(script, targetBranch) {
         def changedPackages = new HashSet<String>()
 
         targetBranch = targetBranch.trim()
-        steps.echo "target branch=[${targetBranch}]"
-        String diffFilesList = steps.sh(script: "git diff ${targetBranch} --name-only", returnStdout: true)
+        script.echo "target branch=[${targetBranch}]"
+        String diffFilesList = script.sh(script: "git diff ${targetBranch} --name-only", returnStdout: true)
         List<String> files = Arrays.asList(diffFilesList.split("\\n"))
     }
 
-    static def releaseVersion(steps, pushUrl, tagNewVersion) {
+    static def releaseVersion(script, pushUrl, tagNewVersion) {
         def version = script.sh (script: "node -p -e \"require('./package.json').version\"", returnStdout: true)
         def name = script.sh (script: "node -p -e \"require('./package.json').name\"", returnStdout: true)
-        env.RELEASE_VERSION = steps.input message: "Current version for ${name} is ${version}. User input required', ok: 'Release!",
+        env.RELEASE_VERSION = script.input message: "Current version for ${name} is ${version}. User input required', ok: 'Release!",
                 parameters: [choice(name: 'RELEASE_VERSION', choices: 'patch\nminor\nmajor', description: 'What is the release version?')]
         def noTagFlag = tagNewVersion ? '' : '--no-git-tag-version'
-        steps.sh "yarn version {$noTagFlag} --new-version ${env.RELEASE_VERSION}"
-        steps.sh "git push ${pushUrl}"
+        script.sh "yarn version {$noTagFlag} --new-version ${env.RELEASE_VERSION}"
+        script.sh "git push ${pushUrl}"
         version = script.sh (script: "node -p -e \"require('./package.json').version\"", returnStdout: true)
         echo "new version is ${version}"
         return version
