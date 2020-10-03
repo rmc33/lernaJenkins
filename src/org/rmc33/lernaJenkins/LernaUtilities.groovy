15133a8a8e39201b300c22ddea28275134e9536f
Ryan Conigliaro <rc@RyansMac.local>
2020-10-02 23:06:04 -0400

    asking user input for version updates

--- src/org/rmc33/lernaJenkins/LernaUtilities.groovy
+++ src/org/rmc33/lernaJenkins/LernaUtilities.groovy
@@ -9,28 +9,27 @@ class LernaUtilities {
         String changedPackages = steps.sh(script: "node_modules/.bin/lerna changed --json", returnStdout: true)
         def jsonSlurper = new JsonSlurper()
         def jsonObjects = jsonSlurper.parseText(changedPackages)
-        return createPackageList(jsonObjects)
+        return jsonObjects.collect {
+            new Package(it.name, it.location)
+        }
     }
 
     static def listChangedPackagesSince(steps, fromBranch) {
         String changedPackages = steps.sh(script: "node_modules/.bin/lerna ls --since ${fromBranch} --json", returnStdout: true)
         def jsonSlurper = new JsonSlurper()
         def jsonObjects = jsonSlurper.parseText(changedPackages)
-        return createPackageList(jsonObjects)
+        return jsonObjects.collect {
+            new Package(it.name, it.location)
+        }
     }
 
     static def listAllPackages(steps) {
         String jsonPackages = steps.sh(script: "node_modules/.bin/lerna list --json", returnStdout: true)
         def jsonSlurper = new JsonSlurper()
         def jsonObjects = jsonSlurper.parseText(jsonPackages)
-        return createPackageList(jsonObjects)
-    }
-
-    private static def createPackageList(jsonObjects) {
-        return jsonObjects.stream().map {
+        return jsonObjects.collect {
             new Package(it.name, it.location)
         }
-        .collect(Collectors.toList())
     }
 
 }
