0a0147c209a00757b1fbb8f0bda7326c510cf80e
Ryan Conigliaro <rc@RyansMac.local>
2020-10-03 21:12:33 -0400

    asking user input for version updates

--- src/org/rmc33/lernaJenkins/LernaPipeline.groovy
+++
@@ -1,8 +0,0 @@
-package org.rmc33.lernaJenkins
-
-public interface LernaPipeline {
-    public List<Package> listChangedPackages(script, config)
-    public void runBeforePackagesPipeline(script, config)
-    public void runPackagePipeline(script, packageProperties, config)
-    public void runAfterPackagesPipeline(script, config)
-}
