8c7b36eaef61216abb6d44b710d7cda65ae4b3e0
Ryan Conigliaro <rc@RyansMac.local>
2020-10-08 23:16:36 -0400

    updating to use config.credentialsId

--- jenkins-pipelines/release-pipeline.groovy
+++ jenkins-pipelines/release-pipeline.groovy
@@ -1,18 +1,18 @@
 
-def runBeforePackagesPipeline(script, config) {
+def runBeforePackagesPipeline(script, branchConiig, config) {
     steps.sh "yarn"
 }
 
-def runPackagePipeline(script, packageProperties, config) {
+def runPackagePipeline(script, packageProperties, branchConfig, config) {
     script.echo "runPipeline ${packageProperties.name}"
     //scan package
     //deploy or publish RC package
 }
 
-def runAfterPackagesPipeline(script, config) {
+def runAfterPackagesPipeline(script, branchConfig, config) {
     //merge to master
     script.input message: 'Approve Merge to maser?', ok: 'Yes'
-    withCredentials([usernamePassword(credentialsId: 'GITHUB_USER', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: 'GITHUB_USER')]) {
         script.sh "git fetch"
         script.sh "git checkout master"
         script.sh "git merge remotes/origin/${env.BRANCH_NAME}"
