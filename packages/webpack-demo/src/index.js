0b0bf8ee5661ec6d02638f7c78d3bca2105b9a0d
Ryan Conigliaro <rmconigliaro@gmail.com>
2021-02-06 14:31:03 -0500

    adding test

--- packages/webpack-demo/src/index.js
+++ packages/webpack-demo/src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack', 'world'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack', 'world', 'test'], ' ');
     return element;
 }
 document.body.appendChild(component());
